36c46ad791ae78553f1c83db431ff95aae438273
Shereen Gamal <shereen.gamal@inovaeg.com>
2020-02-24 19:25:16 +0200

    initial

---
+++ app/src/main/java/edu/mahmoud/motionlayouts/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.mahmoud.motionlayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
